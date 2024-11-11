871db6666347ad83fc284fc2e9a682aee9adaea4
kcasl <sadd21331@naver.com>
2024-11-11 21:04:13 +0900

    first

---
+++ src/main/java/com/dev/core/WindowManager.java
@@ -0,0 +1,48 @@
+package com.dev.core;
+
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.CallbackI;
+
+public class WindowManager {
+
+    public static final float FOV = (float) Math.toRadians(60);
+    public static final float Z_NEAR = 0.01f;
+    public static final float Z_FAR = 1000f;
+
+    private final String title;
+
+    private int width, height;
+    private long window;
+
+    private boolean resize, vSync;
+
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean vSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.vSync = vSync;
+        projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if(!GLFW.glfwInit())
+            throw new IllegalStateException("GLFW 초기화 에러.");
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+
+    }
+}
---
+++ src/main/java/com/dev/test/Launcher.java
@@ -0,0 +1,10 @@
+package com.dev.test;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println(Version.getVersion());
+    }
+}
