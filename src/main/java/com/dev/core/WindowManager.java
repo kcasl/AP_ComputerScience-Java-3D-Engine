de0eca729694aada5115202bcbe6beb7adc95179
kcasl <sadd21331@naver.com>
2024-11-11 23:37:11 +0900

    first

--- src/main/java/com/dev/core/WindowManager.java
+++ src/main/java/com/dev/core/WindowManager.java
@@ -3,8 +3,11 @@ package com.dev.core;
 import org.joml.Matrix4f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.CallbackI;
+import org.lwjgl.system.MemoryUtil;
 
 public class WindowManager {
 
@@ -38,11 +41,70 @@ public class WindowManager {
         GLFW.glfwDefaultWindowHints();
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 2);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
 
+        boolean maximised = false;
+        if(width == 0 || height == 0) {
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+            maximised = true;
+        }
+
+        window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if(window == MemoryUtil.NULL)
+            throw new RuntimeException("GLFW window 생성 실패.");
+
+        GLFW.glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
+           this.width = width;
+           this.height = height;
+           this.setResize(true);
+        });
+
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if(key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE)
+                GLFW.glfwSetWindowShouldClose(window, true);
+        });
+
+        if(maximised)
+            GLFW.glfwMaximizeWindow(window);
+        else {
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos(window, (vidMode.width() - width) / 2,
+                    (vidMode.height() - height) / 2);
+        }
+
+        GLFW.glfwMakeContextCurrent(window);
+
+        if(isvSync())
+            GLFW.glfwSwapInterval(1);
+
+        GLFW.glfwShowWindow(window);
+
+        GL.createCapabilities();
+
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glEnable(GL11.GL_BACK);
     }
+
+
+
+    public boolean isResize() {
+        return resize;
+    }
+
+    public boolean isvSync() {
+        return vSync;
+    }
+
+    public void setResize(boolean resize) {
+        this.resize = resize;
+    }
+
 }
